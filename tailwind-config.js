71e53216fecc58b1c7db91d3cc11bf2b7cc2e78d
Mathieu Bonte <mathieu.bonte@gmail.com>
2025-02-11 12:25:39 +0100

    Lms - initial commit

---
+++ tailwind-config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				"page-bg": "#f9fafb",
+				"text-primary": "#111827",
+				"text-secondary": "#374151",
+				"border-color": "#e5e7eb",
+				"accent-blue": "#2563eb",
+				"accent-blue-hover": "#1d4ed8",
+				"disabled-gray": "#9ca3af", // Gris pour les éléments désactivés
+			},
+			fontFamily: {
+				sans: ["Inter", "sans-serif"],
+			},
+			spacing: {
+				1.5: "0.375rem",
+			},
+		},
+	},
+};
